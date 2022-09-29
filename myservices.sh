ee86e7a533a6421c09b0661f6fca7416d4ac25b8
HabibBG88 <hbg.habib@gmail.com>
2022-09-29 17:49:49 +0200

    update frontend with reverseproxy public load balancer

---
+++ myservices.sh
@@ -0,0 +1,9 @@
+kubectl delete svc backend-user
+kubectl delete svc backend-feed
+kubectl delete svc reverseproxy
+kubectl delete svc frontend
+
+kubectl apply -f feed-service.yaml
+kubectl apply -f user-service.yaml
+kubectl apply -f frontend-service.yaml
+kubectl apply -f reverseproxy-service.yaml
